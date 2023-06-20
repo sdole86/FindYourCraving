ef7a71c89c40a628e6d486add1ce0dfdd5c98cfe
Sam Dole <sdole86@gmail.com>
2023-06-20 16:31:28 -0400

    Added display for nearest selected restaurants

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let map;
 let marker;
+let markers = [];
+let infoWindow;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
@@ -14,6 +16,8 @@ initMap();
 function queryAddress() {
   let address = document.getElementById("address").value;
   let displayElement = document.getElementById("display");
+  let selectionDiv = document.getElementById("type-selection");
+
   if (address) {
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode({ address: address }, function (results, status) {
@@ -28,6 +32,12 @@ function queryAddress() {
         displayElement.textContent =
           "Latitude: " + latitude + ", Longitude: " + longitude;
         // Use the latitude and longitude for further processing
+        selectionDiv.style.display = "block";
+
+        // add listener to button
+        document.getElementById("find-restaurants-btn").addEventListener("click", function() {
+          searchRestaurants();
+        });
 
         map.setCenter(location);
         map.setZoom(15);
@@ -37,31 +47,128 @@ function queryAddress() {
         });
       } else {
         displayElement.textContent =
-          "Geocode was not successful for the following reason: " + status;
+          status + ": Search unsuccessful";
+        selectionDiv.style.display = "none";
       }
     });
   }
 }
 
-function findRestaurants() {
-  var restaurantType = document.getElementById("restaurant-type").value;
-  if (marker) {
-    var markerPosition = marker.getPosition();
-    var request = {
-      location: markerPosition,
-      radius: 5000, // Search radius in meters (adjust as needed)
-      type: ["restaurant"],
-      keyword: restaurantType,
-      rankBy: google.maps.places.RankBy.DISTANCE || 2, // Use the value directly or fallback to the numeric value
-    };
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, function (results, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        console.log("Five Closest Restaurants:");
-        for (var i = 0; i < 5 && i < results.length; i++) {
-          console.log(results[i].name);
-        }
-      }
-    });
+async function searchRestaurants() {
+  let restaurantType = document.getElementById("restaurant-type").value;
+
+  let request = {
+    location: marker.getPosition(),
+    type: 'restaurant',
+    rankBy: google.maps.places.RankBy.DISTANCE,
+  };
+
+  if (restaurantType) {
+    request.keyword = restaurantType;
   }
+
+  let service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, function(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      let restaurants = results.slice(0, 5);
+      let tableBody = document.getElementById("restaurant-table").getElementsByTagName("tbody")[0];
+      tableBody.innerHTML = ""; // Clear existing table rows
+
+      // Clear existing markers on the map
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      restaurants.forEach(function(restaurant) {
+        let name = restaurant.name;
+        let address = restaurant.vicinity;
+        let state = "";
+        let zipCode = "";
+
+        if (restaurant.address_components) {
+          for (let component of restaurant.address_components) {
+            if (component.types.includes("administrative_area_level_1")) {
+              state = component.short_name;
+            }
+            if (component.types.includes("postal_code")) {
+              zipCode = component.long_name;
+            }
+          }
+        }
+
+        let photoUrl = restaurant.photos ? restaurant.photos[0].getUrl() : ""; // Get the URL of the first photo, if available
+        let priceLevel = "$".repeat(restaurant.price_level || 0);
+
+        let rating = restaurant.rating || "N/A";
+        let ratingStars = '';
+        if (rating !== "N/A") {
+          let roundedRating = Math.round(rating * 2) / 2; // Round to nearest half
+          for (let i = 0; i < 5; i++) {
+            if (roundedRating >= i + 0.5) {
+              ratingStars += '<i class="bi bi-star-fill gold-star"></i>'; 
+            } else if (roundedRating >= i) {
+              ratingStars += '<i class="bi bi-star-half gold-star"></i>'; 
+            } else {
+              ratingStars += '<i class="bi bi-star gold-star"></i>'; 
+            }
+          }
+        }
+
+        // Create marker for the restaurant
+        let restaurantLocation = restaurant.geometry.location;
+        let marker = new google.maps.Marker({
+          position: restaurantLocation,
+          map: map,
+        });
+
+        // Create info window for the marker
+        let infoWindowContent = `<div><h4>${name}</h4><p>${address}, ${state} ${zipCode}</p></div>`;
+        infoWindow = new google.maps.InfoWindow({
+          content: infoWindowContent,
+        });
+
+        // Add click event listener to the marker to open the info window
+        marker.addListener('click', function() {
+          infoWindow.open(map, marker);
+        });
+
+        // Add marker to the markers array
+        markers.push(marker);
+
+        let row = tableBody.insertRow();
+        let nameCell = row.insertCell();
+        let locationCell = row.insertCell(); // New cell for combined location field
+        let photoCell = row.insertCell();
+        let priceLevelCell = row.insertCell();
+        let ratingCell = row.insertCell();
+
+        nameCell.innerHTML = `<span class="restaurant-name" onclick="centerMapOnMarker(${restaurantLocation.lat()}, ${restaurantLocation.lng()}, '${name}', '${address}', '${state}', '${zipCode}')">${name}</span>`;
+        locationCell.innerHTML = `${address}, ${state} ${zipCode}`; // Combine address, state, and ZIP code
+        photoCell.innerHTML = photoUrl ? `<img src="${photoUrl}" alt="Restaurant Photo" width="100" onclick="openLightbox('${photoUrl}')">` : "N/A";
+        priceLevelCell.innerHTML = priceLevel;
+        ratingCell.innerHTML = ratingStars;
+      });
+
+      document.getElementById("restaurant-display-table").style.display = "block";
+    }
+  });
+}
+
+function centerMapOnMarker(lat, lng, name, address, state, zipCode) {
+  map.setCenter({ lat: lat, lng: lng });
+  map.setZoom(15);
+  let infoWindowContent = `<div><h4>${name}</h4><p>${address}, ${state} ${zipCode}</p></div>`;
+  infoWindow.setContent(infoWindowContent); // Update the info window content
+  infoWindow.open(map); // Open the info window
+}
+
+
+function openLightbox(imageUrl) {
+  document.getElementById("lightbox-image").src = imageUrl;
+  document.getElementById("lightbox-container").style.display = "flex";
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox-container").style.display = "none";
 }
